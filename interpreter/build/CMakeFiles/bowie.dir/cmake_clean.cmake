92e639c69e8158c7281e49cd788765fda74c0d78
Turtel216 <papadimitrios216@gmail.com>
2024-08-10 13:39:35 +0300

    Added C utils

---
+++ interpreter/build/CMakeFiles/bowie.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bowie.dir/src/main.c.o"
+  "CMakeFiles/bowie.dir/src/main.c.o.d"
+  "bowie"
+  "bowie.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bowie.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
